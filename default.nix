ccd80fbdc17311d510d9e91d450bbc6fbe0c5c23
Christopher Mckay <chris@error.cm>
2019-11-07 00:59:59 +1000

    that didnt work

--- default.nix
+++ default.nix
@@ -55,8 +55,6 @@ in {
       package = pkgs.pulseaudioFull;
       daemon.config = {
         flat-volumes = "no";
-        enable-lfe-remixing = "yes";
-        lfe-crossover-freq = 250;
       };
     };
     bluetooth = {
