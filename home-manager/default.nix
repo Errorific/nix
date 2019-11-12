ccbf13344ade8ad2fd483921276941c934561412
Christopher Mckay <chris@error.cm>
2019-11-13 09:19:31 +1000

    Adding notificaiton tool

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -132,6 +132,55 @@ in {
     network-manager-applet.enable = true;
     xembed-sni-proxy.enable = true;
     blueman-applet.enable = true;
+    flameshot.enable = true;
+    unclutter.enable = true;
+    dunst = {
+      enable = true;
+      settings = {
+        global = {
+          font = "Source Code Pro";
+          markup = "full";
+          format = "<b>%s</b>\\n%b";
+          icon_position = "left";
+          sort = true;
+          alignment = "center";
+          geometry = "500x60-15+49";
+          browser = "/usr/bin/firefox -new-tab";
+          transparency = 10;
+          word_wrap = true;
+          show_indicators = false;
+          separator_height = 2;
+          padding = 6;
+          horizontal_padding = 6;
+          separator_color = "frame";
+          frame_width = 2;
+        };
+        shortcuts = {
+          close = "ctrl+space";
+          close_all = "ctrl+shift+space";
+          history = "ctrl+grave";
+          context = "ctrl+shift+period";
+        };
+        urgency_low = {
+          frame_color = "#3B7C87";
+          foreground = "#3B7C87";
+          background = "#191311";
+          timeout = 4;
+        };
+        urgency_normal = {
+          frame_color = "#5B8234";
+          foreground = "#5B8234";
+          background = "#191311";
+          timeout = 6;
+        };
+        urgency_critical = {
+          frame_color = "#B7472A";
+          foreground = "#B7472A";
+          background = "#191311";
+          timeout = 8;
+        };
+      };
+    };
   };
 
   xsession = {
