be35574741a07af23f567ba8ac0b2d46bb01821e
Christopher Mckay <chris@error.cm>
2019-11-13 10:06:14 +1000

    changing taffy config, bet I broke it

--- home-manager/dotfiles/taffybar/taffybar.hs
+++ home-manager/dotfiles/taffybar/taffybar.hs
@@ -15,28 +15,31 @@ import           System.Taffybar.SimpleConfig                    (Position (Top)
                                                                   toTaffyConfig,
                                                                   widgetSpacing)
 
-import           System.Taffybar.Widget.FreedesktopNotifications (defaultNotificationConfig,
-                                                                  notifyAreaNew)
-import           System.Taffybar.Widget.MPRIS2                   (mpris2New)
 import           System.Taffybar.Widget.SimpleClock
 
 import           System.Taffybar.Widget                          (defaultWorkspacesConfig,
                                                                   sniTrayNew,
                                                                   textBatteryNew,
-                                                                  workspacesNew)
+                                                                  workspacesNew,
+                                                                  layoutNew,
+                                                                  defaultLayoutConfig,
+                                                                  windowsNew,
+                                                                  defaultWindowsConfig,
+                                                                  buildContentsBox)
 
 main =
   let
       clock = textClockNew Nothing "<span size='large' fgcolor='white'>%a %d %b %Y %H:%M:%S</span>" 1
       workspaces = workspacesNew defaultWorkspacesConfig
-      note = notifyAreaNew defaultNotificationConfig
+      layout = layoutNew defaultLayoutConfig
+      windows = windowsNew defaultWindowsConfig
       tray = sniTrayNew
       batt = textBatteryNew "Batt: $percentage$% $status$ $time$"
       cfg = defaultSimpleTaffyConfig
         { barHeight = 40
         , barPosition = Top
         , widgetSpacing = 10
-        , startWidgets = [ workspaces, note ]
+        , startWidgets = workspaces : map (>>= buildContentsBox) [ layout, windows ]
         , endWidgets = [ clock, tray, batt ]
         }
   in
